f5481e08a34998b9ec22c7451038cdc7207afa3f
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-05-20 14:41:24 +0200

    Removed autodeployment to devices

--- 2D/deploy.sh
+++ 2D/deploy.sh
@@ -1,12 +1,1 @@
 node_modules/.bin/gulp deploy
-
-# Give the user some time to abort the script
-echo Will run on device in 3 seconds...
-sleep 1
-printf .
-sleep 1
-printf .
-sleep 1
-printf ".\n"
-
-(cd cordova && cordova run --device)
